557a783a0f64678fd9b87009a57005bc488bb367
paulodesiqueira <paulodesiqueira@gmail.com>
2024-07-05 15:49:30 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,27 +21,31 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	private String longDescrrition;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
 	
 	public Game () {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
-			String shortDescription, String longDescrrition) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescrrition = longDescrrition;
+		this.longDescription = longDescription;
 	}
 
 	public Long getId() {
@@ -76,12 +80,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {
@@ -108,12 +112,12 @@ public class Game {
 		this.shortDescription = shortDescription;
 	}
 
-	public String getLongDescrrition() {
-		return longDescrrition;
+	public String getLongDescription() {
+		return longDescription;
 	}
 
-	public void setLongDescrrition(String longDescrrition) {
-		this.longDescrrition = longDescrrition;
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
 	}
 
 	@Override
